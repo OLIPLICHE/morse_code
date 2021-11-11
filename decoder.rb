7698e81c88dcbcba1af6d123140a31aab9bdf2f7
codecaiine <akabrouyannickn@gmail.com>
2021-11-11 18:17:45 +0000

    fix ruby spaces

--- decoder.rb
+++ decoder.rb
@@ -2,31 +2,31 @@ print 'Enter your morse code: '
 
 morse_str = gets.chomp
 
-dictionary = {
-    'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.',
-    'G' => '--.', 'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
-    'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.', 'Q' => '--.-', 'R' => '.-.', 'S' => '...',
-    'T' => '-', 'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--', 'Z' => '--..',
-    '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-', '5' => '.....', '6' => '-....', '7' => '--...',
-    '8' => '---..', '9' => '----.', '0' => '-----'
-}
-
 def decode_char(character)
-  return $dictionary.key(character) || ' '
+  dictionary = {
+        'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.',
+        'G' => '--.', 'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
+        'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.', 'Q' => '--.-', 'R' => '.-.', 'S' => '...',
+        'T' => '-', 'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--', 'Z' => '--..',
+        '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-', '5' => '.....', '6' => '-....', '7' => '--...',
+        '8' => '---..', '9' => '----.', '0' => '-----'
+  }
+
+  dictionary.key(character) || ' '
 end
 
 def decode_word(morse_text)
   text = ''
     morse_text.split.each do |char| text += decode_char(char)
-    end
-    text
+  end
+  text
 end
 
 def result(morse_result)
   result = []
-    morse_result.split(' ').each do |text|
-      result.push(decode_word)
-    end
+  morse_result.split(' ').each do |word|
+    result.push(decode_word)
+  end
   result.join(' ')
 end
 
