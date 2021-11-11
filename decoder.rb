473c8824cf0f75dc15fc2ccea3bbb430c6b5c8ee
codecaiine <akabrouyannickn@gmail.com>
2021-11-11 20:32:18 +0000

    Remove unnecessary characters and add key word

--- decoder.rb
+++ decoder.rb
@@ -3,31 +3,34 @@ morse_str = gets.chomp
 
 def decode_char(character)
   dictionary = {
-    'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.',
-    'G' => '--.', 'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
-    'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.', 'Q' => '--.-', 'R' => '.-.', 'S' => '...',
-    'T' => '-', 'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--', 'Z' => '--..',
-    '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-', '5' => '.....', '6' => '-....', '7' => '--...',
-    '8' => '---..', '9' => '----.', '0' => '-----'
+    'A' => '.-', 'B' => '-...', 'C' => '-.-.',
+    'D' => '-..', 'E' => '.', 'F' => '..-.',
+    'G' => '--.', 'H' => '....', 'I' => '..',
+    'J' => '.---', 'K' => '-.-', 'L' => '.-..',
+    'M' => '--', 'N' => '-.', 'O' => '---',
+    'P' => '.--.', 'Q' => '--.-', 'R' => '.-.',
+    'S' => '...', 'T' => '-', 'U' => '..-',
+    'V' => '...-', 'W' => '.--', 'X' => '-..-',
+    'Y' => '-.--', 'Z' => '--..'
   }
 
   dictionary.key(character) || ' '
 end
 
 def decode_word(morse_word)
-  text = ''
+  word = ''
   morse_word.split.each do |char|
-    text += decode_char(char)
+    word += decode_char(char)
   end
-  text
+  word
 end
 
 def decode_result(morse_result)
-  decode_result = []
-  morse_result.split.each do |text|
-    decode_result.push(text)
+  result = []
+  morse_result.split('   ').each do |word|
+    result.push(decode_word(word))
   end
-  decode_result.join(' ')
+  result.join(' ')
 end
 
 puts "Your message is: #{decode_result(morse_str)}"
